168945400ac40f3164ba67a80c97d08166c9bae1
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-12 16:39:55 +0100

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -27,6 +27,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
   end
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+require"open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -20,9 +22,13 @@ Ingredient.create(name: "coke")
   cocktails.save!
 end
 
-# 10.times do
-#   ingredients = Ingredient.new({ name: Faker::Food.ingredient })
-#   ingredients.save!
-# end
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+data = JSON.parse(open(url).read)
+data["drinks"].each do |ingredient_attributes|
+  Ingredient.create(name: ingredient_attributes["strIngredient1"])
+end
+
+
 
 
